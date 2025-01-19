dcec788984b3407302a67d3eb53a754c4fb60867
Lars Vonk <lars.vonk@despar.nl>
2025-01-19 13:37:19 +0100

    chore: more randomness added to hash

--- main.c
+++ main.c
@@ -3,59 +3,61 @@
 #include <math.h>
 #include <string.h>
 
-#define HASH_SIZE 32
+#define HASH_SIZE 64
 
 const char HEX_DIGITS[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
 // Constants for the LCG (a * x + c) % m
-const unsigned long long a = 256;        // 2^8
-const unsigned long long c = 65536;      // 2^16
-const unsigned long long m = 4294967296; // 2^32
+const unsigned long long A = 256;        // 2^8
+const unsigned long long C = 65536;      // 2^16
+const unsigned long long M = 4294967296; // 2^32
 
 // Generate a random number using a Linear Congruential Generator (LCG)
 unsigned long long semi_random_number(unsigned long long seed, unsigned long long max_len)
 {
     // Generate a "random" number
-    unsigned long long random_number = (a * seed + c) % m;
+    unsigned long long random_number = (A * seed + C) % M;
 
     return random_number % max_len;
 }
 
 char *hash_str(char *input_str)
 {
-    int i = 0;
-    unsigned long long hash_number;
+    unsigned long long hash_number = 0;
+    unsigned int oscilator = 1;
+    size_t i = 0;
 
     // Preallocate memory for the hash
     char *hash = malloc(HASH_SIZE * sizeof(char));
-
-    unsigned int oscilator = 1;
 
     while (input_str[i] != '\0')
     {
         // Convert the ASCII code to 64-bit long long
         unsigned long long char_code_long = (unsigned long long)input_str[i];
-        char_code_long = char_code_long + semi_random_number(char_code_long + i, 3);
+        char_code_long += semi_random_number(char_code_long + i, 3);
 
         // Repeat 16 bits 4 times to make 64-bit long long
-        const unsigned long long char_code_64 = char_code_long << 48 | char_code_long << 32 | char_code_long << 16 | char_code_long;
+        const unsigned long long char_code_64 = (char_code_long << 48) | (char_code_long << 32) | (char_code_long << 16) | char_code_long;
 
-        // XOR with left-shifted hash
-        hash_number = hash_number ^ char_code_64;
+        // Generate random number to rotate
+        const unsigned int random_rotate = semi_random_number(hash_number, 2);
 
         // Apply bitwise operations
         if (oscilator)
         {
             // Rotate right 8 bits
-            hash_number = (hash_number >> 8) | (hash_number << (64 - 8));
+            hash_number = (hash_number >> random_rotate) | (hash_number << (64 - random_rotate));
             oscilator = 0;
         }
         else
         {
             // Rotate left 4 bits
-            hash_number = (hash_number << 4) | (hash_number >> (64 - 4));
+            hash_number = (hash_number << random_rotate) | (hash_number >> (64 - random_rotate));
             oscilator = 1;
         }
+
+        // XOR with left-shifted hash
+        hash_number ^= char_code_64;
 
         i++;
     }
@@ -65,6 +67,7 @@ char *hash_str(char *input_str)
         hash[i] = HEX_DIGITS[hash_number % 16];
         hash_number = hash_number * 2 / 3;
     }
+    hash[HASH_SIZE] = '\0';
 
     return hash;
 }
@@ -87,6 +90,10 @@ int tests()
     printf("Hash: %s\n", test_hash);
 
     test_str = "b";
+    test_hash = hash_str(test_str);
+    printf("Hash: %s\n", test_hash);
+
+    test_str = "aa";
     test_hash = hash_str(test_str);
     printf("Hash: %s\n", test_hash);
 
